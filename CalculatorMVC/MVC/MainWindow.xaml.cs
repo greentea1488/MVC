f432c0c6c8a257781d7eedf35db3c9476e482d99
greentea1488 <84225678+greentea1488@users.noreply.github.com>
2022-02-02 10:12:48 +0300

    Add files via upload

---
+++ CalculatorMVC/MVC/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double result;
+        double x1, x2;
+        char symbol;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tb = Result;
+            Model.tbCombo = ComboChange;
+            Model.tbxF = ChangeXFirst;
+            Model.tbxS = ChangeXSecond;
+            ComboChange.ItemsSource = Model.dataList;
+
+        }
+
+        private void controller_Click(object sender, RoutedEventArgs e)
+        {
+            if (symbol == '+')
+            {
+                result = x1 + x2;
+            }
+            if (symbol == '-')
+            {
+                result = x1 - x2;
+            }
+            if (symbol == '*')
+            {
+                result = x1 * x2;
+            }
+            if (symbol == '/')
+            {
+                result = x1 / x2;
+            }
+
+            Model.tb.Text = result.ToString();
+        }
+
+        private void ChangeXFirst_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            x1 = Convert.ToDouble(Model.tbxF.Text);
+        }
+
+        private void ChangeXSecond_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            x2 = Convert.ToDouble(Model.tbxS.Text);
+        }
+
+        private void ComboChange_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            symbol = Model.dataList[ComboChange.SelectedIndex];
+            Model.Combo = ComboChange.SelectedIndex;
+        }
+    }
+}
---
+++ CalculatorMVC/MVC/Model.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace MVC
+{
+    static class Model
+    {
+        public static List<char> dataList = new List<char> { '+', '-', '*', '/' };
+        static double result;
+        public static TextBlock tb;
+        public static ComboBox tbCombo;
+        public static TextBox tbxF;
+        public static TextBox tbxS;
+
+        public static int Combo
+        {
+            set
+            {
+                tbCombo.Text = dataList[value].ToString();
+            }
+        }
+
+        public static double Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                tb.Text = result.ToString();
+            }
+        }
+    }
+}
